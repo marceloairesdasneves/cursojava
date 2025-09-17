f66eab1c48580bb8aa9ff7dfb8aec1a971fffdd8
marceloairesdasneves <mneves230@gmail.com>
2025-09-17 18:49:09 -0300

    Finalizando a aula 7 Signed-off-by:marceloairesdasneves <mneves230@gmail.com>

---
+++ src/main/java/br/com/aulasdejava/projetoaulajava/ExemplosDoMetodoMath.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.aulasdejava.projetoaulajava;
+
+/**
+ *
+ * @author USER
+ */
+public class ExemplosDoMetodoMath {
+    public static void main(String[] args) {
+        System.out.println("Mostra o arredondamento: " + Math.round(5.6));
+        System.out.println("Mostra o arredondamento para cima: " + Math.ceil(5.2));
+        System.out.println("Mostra o arredondamento para baixo: " + Math.floor(5.9));
+        System.out.println("Numero aleatorio entre 1 e 100: " + (1 + (int) (Math.random()*100)));
+        
+    }
+    
+}
---
+++ src/main/java/br/com/aulasdejava/projetoaulajava/ManipulandoString.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.aulasdejava.projetoaulajava;
+
+/**
+ *
+ * @author USER
+ */
+public class ManipulandoString {
+    public static void main(String[] args) {
+        String texto = "Java";
+        
+        System.out.println(texto.length());
+        System.out.println(texto.charAt(0));
+        System.out.println(texto.charAt(3));
+        System.out.println(texto.isEmpty());
+        System.out.println("".isEmpty());
+    }
+    
+}
